4b03d86797adf54f91524547a46035de19034200
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-07-20 18:40:45 +0200

    changed require to import

--- app.ts
+++ app.ts
@@ -1,11 +1,10 @@
 import * as express from "express"
 import * as cors from "cors"
+import * as mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-const projectRoutes = require("./routes/projectRoutes");
-const mongoose = require("mongoose");
 
 // const dbURI = `mongodb+srv://marta:${hasloDB.haslo}@cluster0.w2bha.mongodb.net/rekruter?retryWrites=true&w=majority`;
 
