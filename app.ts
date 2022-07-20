6bc65bd72a4851873c5d1924f52bbf15bebec044
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-07-20 22:09:08 +0200

    uninstall mongoose

--- app.ts
+++ app.ts
@@ -1,12 +1,8 @@
 import * as express from "express"
 import * as cors from "cors"
-import * as mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT || 8080;
-
-
-// const dbURI = `mongodb+srv://marta:${hasloDB.haslo}@cluster0.w2bha.mongodb.net/rekruter?retryWrites=true&w=majority`;
 
 // Apply CORS policy
 app.use(cors())
