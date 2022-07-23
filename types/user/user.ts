42b8b15f69aa1da9d5e07741a5140bda38fd5997
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-07-23 17:44:39 +0200

    added types need to login user

--- types/user/user.ts
+++ types/user/user.ts
@@ -1,4 +1,30 @@
-export interface UserLogin {
-    email: string;
-    password: string;
+import {FieldPacket} from "mysql2";
+
+export interface UserLoginRequest {
+	email: string;
+	password: string;
+}
+
+enum Role {
+	admin = "admin",
+	hr = "hr",
+	trainee = "trainee"
+}
+
+export type UserLoginResponseFromDatabase = [
+	{
+		id: string,
+		email: string,
+		password: string,
+		role: Role,
+		currentTokenId: string,
+	}[],
+	FieldPacket[]
+];
+
+export type UserLoginResultsResponse = {
+	isLogin: boolean,
+	token?: string,
+	role?: string,
+	message?: string,
 }
