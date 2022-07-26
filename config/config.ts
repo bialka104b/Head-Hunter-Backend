d0e5c4b17bdcae4b32cfe6b54809e9cf06e0e8b5
Marcus Aurelius <8272331@gmail.com>
2022-07-26 13:07:07 +0200

    config file deleted from .gitignore

---
+++ config/config.ts
@@ -0,0 +1,12 @@
+export const config = {
+	db: {
+		port: 3306,
+		host: '127.0.0.1',
+		user: 'root',
+		password: '123456',
+		database: 'head_hunter_megak',
+	},
+	jwt: {
+		passportSecretOrKey: 'test1234',
+	}
+}
