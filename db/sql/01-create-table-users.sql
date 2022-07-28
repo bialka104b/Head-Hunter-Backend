e801d9a830299b884fc5f38805a203e1447ed64f
Sextus Empiricus <8272331@gmail.com>
2022-07-28 19:10:03 +0200

    registerToken field added to users table

--- db/sql/01-create-table-users.sql
+++ db/sql/01-create-table-users.sql
@@ -2,11 +2,12 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
 (
-    id        VARCHAR(36) PRIMARY KEY                  DEFAULT (UUID()),
-    email     VARCHAR(256) UNIQUE             NOT NULL,
-    password  VARCHAR(72)                     NOT NULL,
-    role      ENUM ('admin', 'hr', 'trainee') NOT NULL,
-	currentTokenId VARCHAR(256)               NULL DEFAULT NULL,
-    createdAt DATE                            NOT NULL DEFAULT (NOW()),
-	isActive  BOOLEAN                         NOT NULL DEFAULT TRUE
+	id             VARCHAR(36) PRIMARY KEY                  DEFAULT (UUID()),
+	email          VARCHAR(256) UNIQUE             NOT NULL,
+	password       VARCHAR(72)                     NOT NULL,
+	role           ENUM ('admin', 'hr', 'trainee') NOT NULL,
+	currentTokenId VARCHAR(256)                             DEFAULT NULL,
+	registerToken  VARCHAR(36)                              DEFAULT NUll,
+	createdAt      DATE                            NOT NULL DEFAULT (NOW()),
+	isActive       BOOLEAN                         NOT NULL DEFAULT TRUE
 );
