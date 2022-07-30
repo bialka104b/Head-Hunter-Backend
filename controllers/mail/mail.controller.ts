62f00898c822a69abf3a7f2f109be784546d3131
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-07-30 17:37:19 +0200

    added mail controller

---
+++ controllers/mail/mail.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { MailRecord } from '../../records/mail/mail.record';git
+import { sendRegisterMail } from '../../mailService/sendMail';
+import { jsonResponse } from '../../utils/jsonResponse';
+import { JsonResponseStatus } from '../../types/api/json-response';
+
+export class MailController {
+	static async registerMail(req: Request, res: Response) {
+		try {
+			const unregisterUsers = await MailRecord.findUnregisterUsers();
+
+			for (const users of unregisterUsers) {
+				const { id, email, registerToken } = users;
+				await sendRegisterMail(email, id, registerToken);
+			}
+
+			res.json(jsonResponse({
+				code: 200,
+				status: JsonResponseStatus.success,
+				message: 'done',
+			}));
+		} catch (e) {
+			console.log(e);
+		}
+	}
+}
