fc62b36429628b098a5371c7b45198a74e34bd1a
Sextus Empiricus <8272331@gmail.com>
2022-07-31 15:45:34 +0200

    user password varchar 72 => 255

--- db/sql/01-create-table-users.sql
+++ db/sql/01-create-table-users.sql
@@ -4,7 +4,7 @@ CREATE TABLE users
 (
 	id             VARCHAR(36) PRIMARY KEY                  DEFAULT (UUID()),
 	email          VARCHAR(256) UNIQUE             NOT NULL,
-	password       VARCHAR(72)                     NOT NULL,
+	password       VARCHAR(255)                     NOT NULL,
 	role           ENUM ('admin', 'hr', 'trainee') NOT NULL,
 	currentTokenId VARCHAR(256)                             DEFAULT NULL,
 	registerToken  VARCHAR(36)                              DEFAULT NUll,
