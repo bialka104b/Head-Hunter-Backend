b0576b9b709f008469980ea6433e155bfb67503e
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-08-03 14:11:41 +0200

    added passport authenticate to endpoint getAllListedTrainees

--- routes/trainees.router.ts
+++ routes/trainees.router.ts
@@ -5,9 +5,9 @@ import passport from 'passport';
 const traineesRouter = express.Router();
 
 traineesRouter
-	.get('/:limit/:currentPage', TraineesController.getAllListedTrainees)
+	.get('/:limit/:currentPage', passport.authenticate('jwt', { session: false }), TraineesController.getAllListedTrainees)
 	.get('/interviewsList/:limit/:currentPage', passport.authenticate('jwt', { session: false }), TraineesController.getInterviewsTraineesList)
-	.get('/:id', passport.authenticate('jwt', { session: false }),TraineesController.getTraineeProfile);
+	.get('/:id', passport.authenticate('jwt', { session: false }), TraineesController.getTraineeProfile);
 
 export {
 	traineesRouter,
