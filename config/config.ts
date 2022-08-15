fd74af9dde15d6088c7bc1fa4cd7d46c30278500
airfortech <airm@xxx.yy>
2022-08-16 00:06:40 +0200

    added mail to config

--- config/config.ts
+++ config/config.ts
@@ -9,10 +9,10 @@ export const config = {
 	jwt: {
 		passportSecretOrKey:
 			'askdjamsjdams djf asdkfj asjdfmasdjfmoasd ajflasdjfio asdufp83[][][]4ijt9420 fmwepa fm -2t u2q490ug qu g-04t m1 q032u59 qw89yt 240q 04q0- fwjavdsmkdnt539q840ewfi odsvjiboq3rpwef9jvnw498pq39ugrjv mpwn4p gjbn',
-		salt: 'aomihdfadfhalmsdj893rh 892rymd 2q89rync2hcodfa;skncasldjkvba9s haiopj; caiodmhf892 h[fp\';alskdoqp\'wd 90wfduqjpefio ;r893gqh fnvwda; lwajscnavupo2\'-31ojwp;nOLDhIEWAVKNSDLu ;pqej2IEO QWF;J ji;LKH;OIHlkhkH D;lkh',
+		salt: "aomihdfadfhalmsdj893rh 892rymd 2q89rync2hcodfa;skncasldjkvba9s haiopj; caiodmhf892 h[fp';alskdoqp'wd 90wfduqjpefio ;r893gqh fnvwda; lwajscnavupo2'-31ojwp;nOLDhIEWAVKNSDLu ;pqej2IEO QWF;J ji;LKH;OIHlkhkH D;lkh",
 	},
 	email: {
-		email: 'megak.gr7@gmail.com',
-		pass: 'zjefjbxwshlhkxvx',
+		email: 'megak.grupa7@gmail.com',
+		pass: 'iFimSisV0y-HYdp7pjDa',
 	},
 };
