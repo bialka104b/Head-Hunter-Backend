9475da9e1cc7d6287c7b65c48f802e50bbc6523a
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-08-19 18:29:53 +0200

    added endpoint cancelHire

--- routes/admin.router.ts
+++ routes/admin.router.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import passport from 'passport';
 import { AdminController } from '../controllers/admin/admin.controller';
 import { upload } from '../importFile/importFile';
+import { TraineesController } from '../controllers/trainees/trainees.controller';
 
 export const adminRouter = Router();
 
 adminRouter
 	.delete('/', passport.authenticate('jwt', { session: false }), AdminController.deleteUser)
+	.get('/cancelHire/:traineeId', passport.authenticate('jwt', { session: false }), TraineesController.cancelHired)
 	.post('/importTrainees', passport.authenticate('jwt', { session: false }), upload.single('file'), AdminController.importTraineesFromCsvFile);
 
