91eebd243176a94bfdd701bccc06323b6478457b
airfortech <airm@xxx.yy>
2022-08-20 11:23:51 +0200

    fixed conflicts

--- routes/hr.router.ts
+++ routes/hr.router.ts
@@ -4,10 +4,11 @@ import { hrController } from '../controllers/hr/hr.controller';
 
 export const hrRouter = Router();
 
-hrRouter
-  .post(
-	  '/editProfile',
-	  passport.authenticate('jwt', { session: false }),
-	  hrController.editProfile,
+hrRouter.post(
+	'/editProfile',
+	passport.authenticate('jwt', { session: false }),
+	() => {
+		console.log('fix it!');
+	},
+	// hrController.editProfile,
 );
-
