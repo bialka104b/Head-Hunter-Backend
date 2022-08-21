3b6a7597a0f8ce7e05f24aef29d6a47084c4e1ed
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-08-21 14:23:06 +0200

    db:build npm method

---
+++ db/db-builder/src/data/utils/hashedPassword.ts
@@ -0,0 +1,2 @@
+/*password: test1234*/
+export const hashedPassword = '782c3d9e9d9a5282f4da63681a9d7d064cb19b7fd8449ee62d8c52aac56e44ea936ae0436799a64fcae86711f84dbec58910dd8340d97309b3b6ce0727ad5b5c';
