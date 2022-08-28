93ae45293ea73d220f324b4e193ff08ee5496f15
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-08-28 16:07:02 +0200

    added hashPassword to hashedPassword

--- db/db-builder/src/data/utils/hashedPassword.ts
+++ db/db-builder/src/data/utils/hashedPassword.ts
@@ -1,2 +1,4 @@
+import { hashPassword } from '../../../../../utils/hashPassword';
+
 /*password: test1234*/
-export const hashedPassword = '782c3d9e9d9a5282f4da63681a9d7d064cb19b7fd8449ee62d8c52aac56e44ea936ae0436799a64fcae86711f84dbec58910dd8340d97309b3b6ce0727ad5b5c';
+export const hashedPassword = hashPassword('test1234')
