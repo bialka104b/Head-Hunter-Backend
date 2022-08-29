80dd30c8daf80326b829419d1ad0f87e3d2dab09
airfortech <airm@xxx.yy>
2022-08-29 17:34:05 +0200

    fixed creating db

--- db/db-builder/src/create-db.ts
+++ db/db-builder/src/create-db.ts
@@ -16,10 +16,13 @@ const initPool = createPool({
 export const createDatabase = async () => {
 	try {
 		console.log('DATABASE:');
-		await initPool.execute('DROP DATABASE IF EXISTS `m24389_head-hunter`');
-		await initPool.execute(`DROP DATABASE IF EXISTS ${config.db.database}`);
+		await initPool.execute(
+			`DROP DATABASE IF EXISTS ${'`'}${config.db.database}${'`'}`,
+		);
 		console.log(`\t✔ ${config.db.database} database dropped`);
-		await initPool.execute(`CREATE DATABASE IF NOT EXISTS ${config.db.database}
+		await initPool.execute(`CREATE DATABASE IF NOT EXISTS ${'`'}${
+			config.db.database
+		}${'`'}
 		CHARACTER SET utf8mb4
 		COLLATE utf8mb4_unicode_ci`);
 		console.log(`\t✔ ${config.db.database} database created`);
